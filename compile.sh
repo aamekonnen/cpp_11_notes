92317409bab4599d462725ecb455bd9a51c33f58
alhayat <alhayat.ali@gmail.com>
2017-08-24 13:52:16 +0200

    structuring of source files and tweaks

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 build () {
     echo "Building source $1"
     out_binary="${1%.*}"
-    g++ -std=c++0x -Wall -Wextra -Werror $1 -o ${out_binary}
+    g++ -std=c++0x -Wall -Wextra -Werror $1 -o ${out_binary}.out
 }
 
 # build the input source file with g++
